0b97a1aa92b189a470607d3433d2b3922161dfef
Md Sikandar <mdsikandar9226@gmail.com>
2023-03-23 19:02:47 +0530

    added scan from device

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,28 @@ const dialog = document.querySelector("#dialog");
 const openDialogBtn = document.getElementById("openDia");
 const closeDialogBtn = document.getElementById("closeDia");
 const resultContent = document.getElementById("dCont")
-var result;
+const scanImage = document.querySelector(".image-scan")
+var resultt;
 
 let scanner = new Instascan.Scanner({ video: document.getElementById('pre') });
       scanner.addListener('scan', function (content) {
         console.log(content);
         resultContent.innerHTML = content;
-        result = content;
+        resultt = content;
         dialog.showModal();
+        scanner.stop();
       });
 
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
           console.log("Camera Length :"+cameras.length)
           if(cameras.length == 2 ){
-            scanner.start(cameras[1]);
+            let firefoxAgent = userAgentString.indexOf("Firefox") > -1;
+            if(!firefoxAgent){
+              scanner.start(cameras[1]);
+            }else {
+              scanner.start(cameras[0]);
+            }
           }else {scanner.start(cameras[0]);}
         } else {
           console.error('No cameras found.');
@@ -27,11 +34,50 @@ let scanner = new Instascan.Scanner({ video: document.getElementById('pre') });
 
       openDialogBtn.addEventListener("click", ()=> {
         dialog.close();
-        window.open(result, '_blank');
+        scanner.start();
+        window.open(resultt, '_blank');
       })
 
       closeDialogBtn.addEventListener("click",()=> {
         dialog.close();
+        scanner.start()
       })
+
+
+      scanImage.addEventListener("click", () => {
+        const fileInput = document.createElement("input");
+        fileInput.type = "file";
+        fileInput.accept = "image/*";
+        fileInput.style.display = "none";
+
+        fileInput.onchange = () => {
+          const file = fileInput.files[0];
+          const reader = new FileReader();
+
+          reader.onload = () => {
+            const imageData = reader.result;
+            console.log(imageData)
+
+            const codeReader = new ZXingBrowser.BrowserQRCodeReader();
+
+            codeReader.decodeFromImageUrl(imageData)
+              .then(result => {
+                resultContent.innerHTML = result.text;
+                resultt = result.text;
+                console.log("The Result is "+result)
+                dialog.showModal();
+                scanner.stop();
+              })
+              .catch(error => {
+                
+                alert(`QR code could not be scanned:`);
+              });
+          };
+          reader.readAsDataURL(file);
+        };
+        document.body.appendChild(fileInput);
+    
+        fileInput.click();
+      });
 
 
